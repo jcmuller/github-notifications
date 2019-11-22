83983970b02e96027d46ef3570d3462405f5c0bb
Juan C. Müller <jcmuller@gmail.com>
2019-11-22 14:38:53 -0500

    Change config

--- cmd/read-notifications/main.go
+++ cmd/read-notifications/main.go
@@ -13,12 +13,28 @@ import (
 	"github.com/jcmuller/github-notifications/internal/utils"
 )
 
-// Subject holds the subject
-type Subject struct {
-	Title string `json:"title"`
-	Type  string `json:"type"`
-	URL   string `json:"url"`
-}
+type (
+	// Notification holds the notification
+	Notification struct {
+		When        time.Time  `json:"updated_at"`
+		Reason      string     `json:"reason"`
+		Subject     Subject    `json:"subject"`
+		Description string     `json:"description"`
+		Repository  Repository `json:"repository"`
+	}
+
+	// Subject holds the subject
+	Subject struct {
+		Title string `json:"title"`
+		Type  string `json:"type"`
+		URL   string `json:"url"`
+	}
+
+	Repository struct {
+		Name     string `json:"name"`
+		FullName string `json:"full_name"`
+	}
+)
 
 func (s *Subject) String() string {
 	url, err := url.Parse(s.URL)
@@ -32,25 +48,17 @@ func (s *Subject) String() string {
 
 	s.URL = url.String()
 
-	return fmt.Sprintf("[%s] %s -- %s", s.Type, s.Title, s.URL)
+	return fmt.Sprintf("%s -- %s", s.URL, s.Title)
 }
 
 func (n *Notification) String() string {
-	return fmt.Sprintf("%s: [%s]%s %s",
+	return fmt.Sprintf("%s: [%s] [%s] %s %s",
 		fmtDuration(time.Since(n.When)),
+		n.Repository.Name,
 		n.Reason,
 		n.Subject.String(),
 		n.Description,
 	)
-}
-
-// Notification holds the notification
-type Notification struct {
-	When        time.Time `json:"updated_at"`
-	Unread      bool      `json:"unread"`
-	Reason      string    `json:"reason"`
-	Subject     Subject   `json:"subject"`
-	Description string    `json:"description"`
 }
 
 func fmtDuration(d time.Duration) string {
@@ -88,6 +96,7 @@ func main() {
 
 	var notifications []Notification
 	err = json.Unmarshal(body, &notifications)
+	//fmt.Printf("%s", body)
 	if err != nil {
 		log.Fatalf("Error unmarshaling response body: %v\n", err)
 	}
